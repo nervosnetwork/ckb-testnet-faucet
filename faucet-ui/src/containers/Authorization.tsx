96d66923048757fa8456384ebfd053cef7fe8d92
cezres <cezr@sina.com>
2019-03-07 22:59:34 +0800

    feat(ui): verify available

--- faucet-ui/src/containers/Authorization.tsx
+++ faucet-ui/src/containers/Authorization.tsx
@@ -5,7 +5,7 @@ import { ClientId } from '../utils/const'
 export default () => {
   const [enable, setEnbale] = React.useState(false)
 
-  const onClickCheckButton = () => {
+  const onChangeEnable = () => {
     setEnbale(!enable)
   }
 
@@ -13,7 +13,7 @@ export default () => {
     if (!enable) {
       return
     }
-    window.location.href=`https://github.com/login/oauth/authorize?client_id=${ClientId}&state=${window.location.host}`;
+    window.location.href=`https://github.com/login/oauth/authorize?client_id=${ClientId}&state=${window.location.origin}`;
   }
 
   return (
@@ -22,7 +22,7 @@ export default () => {
       <Text size="16px">To get some test token, please click the button below to login with your GitHub ID.</Text>
       <Text size="16px">Each account can only request test token once ever 24 hours.</Text>
       <Box align="center" pad="small">
-        <CheckBox onClick={onClickCheckButton} checked={enable} label="I understand this is for getting test tokens instead of official CKB" />
+        <CheckBox checked={enable} onChange={onChangeEnable} label="I understand this is for getting test tokens instead of official CKB" />
       </Box>
       <Button onClick={onLoginWithGithub} label="Login with Github" />
     </Box>
