7f928b906ac7cc6b8db6b70ecacfbde31b5bd950
cezres <cezr@sina.com>
2019-03-07 12:58:16 +0800

    setup faucet-service project

---
+++ faucet-service/Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "faucet-service",
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),        
+        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.11.5"),
+    ],
+    targets: [
+        .target(name: "App", dependencies: ["Vapor", "SQLite"]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"])
+    ]
+)
+
