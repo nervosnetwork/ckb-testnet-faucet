15b1bbd458a0ec4788c65790faf53d5d76e872c3
cezres <cezr@sina.com>
2019-03-07 23:10:24 +0800

    feat(server): support Github OAuth

---
+++ faucet-service/Sources/App/Utils/DictionaryExtensions.swift
@@ -0,0 +1,24 @@
+//
+//  DictionaryExtensions.swift
+//  App
+//
+//  Created by 翟泉 on 2019/3/7.
+//
+
+import Foundation
+
+extension Dictionary {
+    var toJson: String {
+        guard let jsonData = try? JSONSerialization.data(withJSONObject: self, options: .prettyPrinted) else { return "" }
+        return String(data: jsonData, encoding: .utf8) ?? ""
+    }
+
+    var urlParameters: String {
+        var string = ""
+        for (_, element) in self.enumerated() {
+            string += "\(element.key)=\(element.value)&"
+        }
+        string.removeLast()
+        return string
+    }
+}
