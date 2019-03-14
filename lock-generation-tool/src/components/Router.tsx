7bdb26af074fa1f2f93ad0fa980b1dc07fc9b742
LuFP <lfp@cryptape.com>
2019-03-14 19:10:15 +0800

    feat: add copy-to-clipboard

--- lock-generation-tool/src/components/Router.tsx
+++ lock-generation-tool/src/components/Router.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../containers/Home';
 import { Routes } from '../utils/const';
+import NewLock from '../containers/NewLock';
 
 export default () => {
   return(
     <BrowserRouter>
       <Switch>
+        <Route path={Routes.NewLock} component={NewLock}/>
         <Route path={Routes.Home} component={Home}/>
       </Switch>
     </BrowserRouter>
