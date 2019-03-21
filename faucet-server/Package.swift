ce6cbb9a591451d902b474c44122b92fd7edeefe
cezres <cezr@sina.com>
2019-03-21 16:27:51 +0800

    chore(server): update package dependencies

--- faucet-server/Package.swift
+++ faucet-server/Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),        
         .package(url: "https://github.com/cezres/SQLite.swift.git", from: "0.11.6"),
-        .package(url: "https://github.com/cezres/ckb-sdk-swift.git", .branch("Support-Swift-Package-Manager"))
+        .package(url: "https://github.com/nervosnetwork/ckb-sdk-swift", .revision("4ac2e1e3dcfe723bf6327cdb56f489a23841e303"))
     ],
     targets: [
         .target(name: "App", dependencies: [
