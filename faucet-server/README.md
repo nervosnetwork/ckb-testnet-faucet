a33749c7460347b287a2b63d29cda0d6baf07257
cezres <cezr@sina.com>
2019-03-25 18:44:39 +0800

    chore(server): update readme

--- faucet-server/README.md
+++ faucet-server/README.md
@@ -1,5 +1,20 @@
 ## faucet-service
 
+## Build dependencies
+
+- Swift 4.2
+- libsodium `brew install libsodium`
+
+## Build
+
+- `git clone https://github.com/nervosnetwork/ckb-testnet-faucet.git`
+- `cd ./ckb-testnet-faucet/faucet-server`
+- `swift build`
+
+## Run
+
+`./.build/debug/Run --env dev --github_oauth_client_id <client_id> --github_oauth_client_secret <client_secret>`
+
 ## API
 
 #### Request:
--- faucet-server/Sources/App/boot.swift
+++ faucet-server/Sources/App/boot.swift
@@ -2,7 +2,7 @@ import Vapor
 
 /// Called after your application has initialized.
 public func boot(_ app: Application) throws {
-    let clientid = try app.environment.commandInput.parse(option: .value(name: "github_oauth_clientid")) ?? ""
-    let clientSecret  = try app.environment.commandInput.parse(option: .value(name: "github_oauth_secret")) ?? ""
+    let clientid = try app.environment.commandInput.parse(option: .value(name: "github_oauth_client_id")) ?? ""
+    let clientSecret  = try app.environment.commandInput.parse(option: .value(name: "github_oauth_client_secret")) ?? ""
     GithubOAuth.config(clientId: clientid, clientSecret: clientSecret)
 }
