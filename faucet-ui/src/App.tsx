cf8fef23abcbf07c46f7f080eafcad2c8aaab240
LuFP <lfp@cryptape.com>
2019-04-01 18:40:40 +0800

    feat: fix content positon bug

--- faucet-ui/src/App.tsx
+++ faucet-ui/src/App.tsx
@@ -1,24 +1,33 @@
 import { Box, Heading, Stack } from 'grommet';
 import * as React from 'react';
+import styled from 'styled-components';
 import Router from './components/Router';
 import Theme from './components/Theme';
 
+const Video = styled.video`
+  object-fit: fill;
+  height: 100%;
+  width: 100%;
+`
+
 const App = () => {
   return (
-    <Theme>
-      <Stack>
+    <Box height="100vh">
+      <Stack fill>
         <img src="//cdn.cryptape.com/videos/nervos_poster.png" width="100%"></img>
-        <video autoPlay loop>
+        <Video autoPlay loop>
           <source src="//cdn.cryptape.com/videos/nervos_org_bg.mp4" type="video/mp4" />
-        </video>
-        <Box>
-          <Box align="center" pad={{ top: "large", bottom: "medium" }}>
-            <Heading color="white">Nervos Testnet Faucet</Heading>
+        </Video>
+        <Theme>
+          <Box>
+            <Box align="center" pad={{ top: "large", bottom: "medium" }}>
+              <Heading color="white">Nervos Testnet Faucet</Heading>
+            </Box>
+            <Router />
           </Box>
-          <Router />
-        </Box>
+        </Theme>
       </Stack>
-    </Theme>
+    </Box>
   )
 }
 
