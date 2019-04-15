1e33079fe9ef6defb4f78de584cd166bf555afa1
cezres <cezr@sina.com>
2019-04-15 18:14:56 +0800

    chore: add Base32

--- faucet-server/Package.swift
+++ faucet-server/Package.swift
@@ -8,13 +8,15 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/cezres/SQLite.swift.git", from: "0.11.6"),
-        .package(url: "https://github.com/cezres/ckb-sdk-swift", .revision("d91e2c5484770e0fef7344e24b731b08a97c7e10"))
+        .package(url: "https://github.com/cezres/ckb-sdk-swift", .revision("d91e2c5484770e0fef7344e24b731b08a97c7e10")),
+        .package(url: "https://github.com/norio-nomura/Base32", from: "0.7.0"),
     ],
     targets: [
         .target(name: "App", dependencies: [
             "Vapor",
             "SQLite",
-            "CKB"
+            "CKB",
+            "Base32"
             ]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
