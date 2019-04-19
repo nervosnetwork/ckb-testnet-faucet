e5c2fe5b908219a23c4be2ecf171925bdc56e00e
cezres <cezr@sina.com>
2019-04-19 16:06:38 +0800

    feat: remove base32

--- faucet-server/Package.swift
+++ faucet-server/Package.swift
@@ -8,16 +8,13 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/cezres/SQLite.swift.git", from: "0.11.6"),
-//        .package(url: "https://github.com/nervosnetwork/ckb-sdk-swift", .revision("a649c7fa74027562fc6fa897f66ac22feab25afd")),
-        .package(path: "/Users/tekisen/Documents/GitHub/ckb-sdk-swift"),
-        .package(url: "https://github.com/norio-nomura/Base32", from: "0.7.0"),
+        .package(url: "https://github.com/cezres/ckb-sdk-swift", .revision("218b1d6bc77aae5679a3ebf88a780d175e03aeac")),
     ],
     targets: [
         .target(name: "App", dependencies: [
             "Vapor",
             "SQLite",
             "CKB",
-            "Base32"
             ]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
