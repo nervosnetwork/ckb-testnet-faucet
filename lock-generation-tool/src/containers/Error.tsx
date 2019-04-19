85b3d37ef03491d7268fb2b6bf91ae0ba77d2d85
cezres <cezr@sina.com>
2019-04-19 16:46:21 +0800

    feat(tool):  After the failed page is refreshed, can return to the home page

--- lock-generation-tool/src/containers/Error.tsx
+++ lock-generation-tool/src/containers/Error.tsx
@@ -1,7 +1,16 @@
 import { Box, Text } from 'grommet';
 import * as React from 'react';
+import { Routes } from '../utils/const';
+import { APIHost } from '../utils/const';
 
-export default () => {
+export default (props: any) => {
+  React.useEffect(() => {
+    fetch(`${APIHost}`).then((_response) => {
+      props.history.push({ pathname: Routes.Home })
+    }).catch(() => {
+    })
+  }, [])
+
   return (
     <Box align='center' gap='small'>
       <Text color="text" size="16px">Sorry, the server may have something wrong.</Text>
