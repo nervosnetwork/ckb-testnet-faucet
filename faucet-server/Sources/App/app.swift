577027b05c29a5c147428655944255f4552d575c
cezres <cezr@sina.com>
2019-05-10 14:53:42 +0800

    chore(server): Update app

--- faucet-server/Sources/App/app.swift
+++ faucet-server/Sources/App/app.swift
@@ -22,21 +22,17 @@ public class App {
         services.register(router, as: Router.self)
 
         // Register middleware
-        var middlewares = MiddlewareConfig() // Create _empty_ middleware config
-        // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
-        middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
+        var middlewares = MiddlewareConfig()
+        middlewares.use(ErrorMiddleware.self)
         services.register(middlewares)
 
         try Environment.Process.configure(&env)
     }
 
     private func setupRoutes(_ router: Router) throws {
-        // Basic "It works" example
         router.get { req in
             return "It works!"
         }
-
-        // Basic "Hello, world!" example
         router.get("hello") { req in
             return "Hello, world!"
         }
