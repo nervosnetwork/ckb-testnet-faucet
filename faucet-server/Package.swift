c171ef968488b8f99ee5ee370262675699f1c33c
cezres <cezr@sina.com>
2019-05-14 14:33:18 +0800

    chore(server): Upgrade ckb sdk version

--- faucet-server/Package.swift
+++ faucet-server/Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/cezres/SQLite.swift.git", from: "0.11.6"),
-        .package(url: "https://github.com/cezres/ckb-sdk-swift", .revision("e8d2205fa2105f3855a6c535ab3a679c35b147b3")),
+        .package(url: "https://github.com/cezres/ckb-sdk-swift", .revision("df1c1d0bacdda8550d84f343121822b9c480dcf2")),
     ],
     targets: [
         .target(name: "App", dependencies: [
