578738383d4ba969055cd600d96a2f17fbcddac0
Haichao <haichaozhu77@gmail.com>
2019-05-16 19:46:38 +0800

    updaet error message

--- faucet-ui/src/containers/ServiceError.tsx
+++ faucet-ui/src/containers/ServiceError.tsx
@@ -21,8 +21,7 @@ export default (props: any) => {
 
   return (
     <Box align='center' gap='small'>
-      <Text textAlign='center' color="text" size='xxlarge'>There's something wrong with your internet connection.
-      Please try again later.</Text>
+      <Text textAlign='center' color="text" size='xxlarge'>There's something wrong with your internet connection. Please try again later.</Text>
     </Box>
   )
 }
