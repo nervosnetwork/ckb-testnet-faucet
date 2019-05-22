0b6b2295fc0a488de94a376b06dd999dc9d82658
cezres <cezr@sina.com>
2019-05-22 19:41:26 +0800

    feat(server): Add mysql dependencies

--- faucet-server/Package.swift
+++ faucet-server/Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "faucet-server",
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0"),
         .package(url: "https://github.com/cezres/SQLite.swift.git", from: "0.11.6"),
         .package(url: "https://github.com/nervosnetwork/ckb-sdk-swift", .revision("96285ddf8460ec43b2a612fda4615abe7e41393b")),
     ],
@@ -15,6 +16,7 @@ let package = Package(
             "Vapor",
             "SQLite",
             "CKB",
+            "FluentMySQL",
             ]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
