1ee5d77671787f3427c00af2ba60215a9bb6006d
cezres <cezr@sina.com>
2019-05-22 19:46:02 +0800

    feat(server): Add database configuration

--- faucet-server/Sources/App/App.swift
+++ faucet-server/Sources/App/App.swift
@@ -1,4 +1,7 @@
 import Vapor
+import MySQL
+import Fluent
+import FluentMySQL
 
 public class App {
     private var app: Application!
@@ -17,16 +20,40 @@ public class App {
 
     private func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
         try Environment.Process.configure(&env)
-        
-        // Register routes to the router
+
+        /// Register routes to the router
         let router = EngineRouter.default()
         try setupRoutes(router)
         services.register(router, as: Router.self)
 
-        // Register middleware
+        /// Register middleware
         var middlewares = MiddlewareConfig()
         middlewares.use(ErrorMiddleware.self)
         services.register(middlewares)
+
+        /// Register the configured MySQL database to the database config.
+        try services.register(FluentMySQLProvider())
+        let config = MySQLDatabaseConfig(
+            hostname: Environment.Process.dbHostname,
+            port: Environment.Process.dbPort,
+            username: Environment.Process.dbUsername,
+            password: Environment.Process.dbPassword,
+            database: Environment.Process.dbDatabase,
+            transport: .unverifiedTLS
+        )
+        let mysql = MySQLDatabase(config: config)
+        var databases = DatabasesConfig()
+        databases.add(database: mysql, as: .mysql)
+        services.register(databases)
+
+        /// Configure migrations
+        var migrations = MigrationConfig()
+        services.register(migrations)
+
+        // Configure the rest of your application here
+        var commandConfig = CommandConfig.default()
+        commandConfig.useFluentCommands()
+        services.register(commandConfig)
     }
 
     private func setupRoutes(_ router: Router) throws {
