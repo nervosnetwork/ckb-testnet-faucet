94f5c92fa0c2101609d454816a50a9be41e8bede
LuFP <lfp@cryptape.com>
2019-05-24 10:41:18 +0800

    fix：fix foratting issue

--- faucet-ui/src/containers/Authorization.tsx
+++ faucet-ui/src/containers/Authorization.tsx
@@ -13,16 +13,16 @@ export default () => {
   }
 
   return (
-    <Box align="center" gap="small" pad={{ "left": "xlarge", "right": "xlarge"}}>
-            <ul>
-                <li><Text color="text" size="xlarge">Sign in with your GitHub account to get Testnet tokens for free.</Text></li>
-                <li><Text color="text" size="xlarge">Please note that each account can only request tokens once every 24 hours.</Text></li>
-                <li><Text color="text" size="xlarge">For more information, please refer to the <Anchor href='https://docs.nervos.org' color='brand' target='_blank' >documentation website</Anchor>.</Text></li>
-            </ul>
-            <Box align="start" pad="small">
-                <CheckBox checked={enable} onChange={onChangeEnable} label="I understand that I am requesting for Testnet tokens instead of official Nervos tokens." />
-            </Box>
-            <Button disabled={!enable} primary onClick={onLoginWithGitHub} label="Log in with GitHub" />
-        </Box>
+    <Box align="center" gap="small" pad={{ "left": "xlarge", "right": "xlarge" }}>
+      <ul>
+        <li><Text color="text" size="xlarge">Sign in with your GitHub account to get Testnet tokens for free.</Text></li>
+        <li><Text color="text" size="xlarge">Please note that each account can only request tokens once every 24 hours.</Text></li>
+        <li><Text color="text" size="xlarge">For more information, please refer to the <Anchor href='https://docs.nervos.org' color='brand' target='_blank' >documentation website</Anchor>.</Text></li>
+      </ul>
+      <Box align="start" pad="small">
+        <CheckBox checked={enable} onChange={onChangeEnable} label="I understand that I am requesting for Testnet tokens instead of official Nervos tokens." />
+      </Box>
+      <Button disabled={!enable} primary onClick={onLoginWithGitHub} label="Log in with GitHub" />
+    </Box>
   )
 }
