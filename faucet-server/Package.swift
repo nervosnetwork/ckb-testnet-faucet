b9d1e124549a884a31fc403be80b50e9697ec095
cezres <cezr@sina.com>
2019-07-29 18:51:13 +0800

    chore: Fix transaction error

--- faucet-server/Package.swift
+++ faucet-server/Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0"),
-        .package(url: "https://github.com/nervosnetwork/ckb-sdk-swift", from: "0.17.0")
+        .package(url: "https://github.com/cezres/ckb-sdk-swift", .revision("0740019586e8283ea7b791c23a8c7b6c9cfa1ea7"))
     ],
     targets: [
         .target(name: "App", dependencies: [
