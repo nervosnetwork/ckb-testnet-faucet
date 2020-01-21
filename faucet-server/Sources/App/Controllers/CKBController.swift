1a5409b03299e90def6fe30381c554593f302a00
muxueqz <zhangmingyuan240@gmail.com>
2020-01-21 14:36:54 +0800

    feat: add payment_test api

--- faucet-server/Sources/App/Controllers/CKBController.swift
+++ faucet-server/Sources/App/Controllers/CKBController.swift
@@ -15,6 +15,7 @@ public class CKBController: RouteCollection {
 
     public func boot(router: Router) throws {
         router.get("ckb/faucet", use: faucet)
+        router.get("ckb/faucet_min", use: faucet)
     }
 
     func faucet(_ req: Request) throws -> Future<Response> {
@@ -33,7 +34,8 @@ public class CKBController: RouteCollection {
                         throw APIError(code: status)
                     }
                 }.map { _ in
-                    return try self.sendCapacity(address: content.address, req: req)
+                    return try self.sendCapacity(address: content.address,
+                                  req: req, amount: Environment.CKB.sendCapacityCount)
                 }.map { (txHash: H256) -> H256 in
                     _ = Faucet(userId: user.id, txHash: txHash).save(on: req)
                     _ = self.authService.recordReceivedDate(for: user.id, on: req)
@@ -47,10 +49,41 @@ public class CKBController: RouteCollection {
         }.supportJsonp(on: req)
     }
 
-    private func sendCapacity(address: String, req: Request) throws -> H256 {
+    func faucet_min(_ req: Request) throws -> Future<Response> {
+        return try FaucetRequestContent.decode(from: req).flatMap { (content) -> EventLoopFuture<Response> in
+            guard let accessToken = content.accessToken else { throw APIError(code: .unauthenticated) }
+            guard self.faucetSending.firstIndex(of: accessToken) == nil else {
+                throw Abort(HTTPStatus.badRequest)
+            }
+            self.faucetSending.append(accessToken)
+
+            return GithubService.userInfo(for: accessToken, on: req).unwrap(or: APIError(code: .unauthenticated)).flatMap { (user) -> EventLoopFuture<Response> in
+                return self.authService.verify(userId: user.id, on: req).map { status -> Void in
+                    if status == .ok {
+                        return
+                    } else {
+                        throw APIError(code: status)
+                    }
+                }.map { _ in
+                    return try self.sendCapacity(address: content.address,
+                                  req: req, amount: Capacity(1))
+                }.map { (txHash: H256) -> H256 in
+                    _ = Faucet(userId: user.id, txHash: txHash).save(on: req)
+                    _ = self.authService.recordReceivedDate(for: user.id, on: req)
+                    return txHash
+                }.flatMap { txHash -> EventLoopFuture<Response> in
+                    return try FaucetResponseContent(txHash: txHash).makeJson(for: req)
+                }
+            }.always {
+                self.faucetSending.remove(at: accessToken)
+            }
+        }.supportJsonp(on: req)
+    }
+
+    private func sendCapacity(address: String, req: Request, amount: Capacity) throws -> H256 {
         do {
             let wallet = try Wallet(nodeUrl: URL(string: Environment.CKB.nodeURL)!, privateKey: Environment.CKB.walletPrivateKey)
-            return try wallet.sendTestTokens(to: address, amount: Environment.CKB.sendCapacityCount)
+            return try wallet.sendTestTokens(to: address, amount: amount 
         } catch {
             let logger = try? req.sharedContainer.make(Logger.self)
             logger?.log(req.description + "\n\t" + error.localizedDescription, at: .verbose, file: #file, function: #function, line: #line, column: #column)
